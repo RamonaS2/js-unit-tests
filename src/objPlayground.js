a59b9019c8b762be7b1de7bc29bd88d5dd526554
RamonaS2 <bandeiraramona2@gmail.com>
2022-03-23 00:53:25 -0300

    requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+// Alguns sites utilizados: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round.
+const calculator = (number1, number2) => {
+  let calculos = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
 
-const arrayGenerator = (type, object) => {};
+  return calculos;
+};
+
+const arrayGenerator = (type, object) => {
+if (type === 'keys') {
+return Object.keys(object);
+} if (type === 'values') {
+return Object.values(object);
+} if (type === 'entries') {
+return Object.entries(object);
+}
+};
 
 module.exports = { calculator, arrayGenerator };
